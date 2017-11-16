7d884b71ab9a2e0e2d027dc37965135a13c1dac8
Denis <burov8842@gmail.com>
2017-11-17 00:12:02 +0300

    path (wip)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : Singleton<GameManager> {
 
     private void FixedUpdate()
     {
+        ShowPath();
         foreach (SpaceObject spaceObject in spaceObjects)
         {
             Vector2 resultGravityForce = Vector2.zero;
@@ -26,13 +27,58 @@ public class GameManager : Singleton<GameManager> {
                 if (spaceObject == otherSpaceObject)
                     continue;
 
-                float distance = Vector2.Distance(spaceObject.transform.position, otherSpaceObject.transform.position);
-                float gravityForceValue = G * spaceObject.GetComponent<Rigidbody2D>().mass * otherSpaceObject.GetComponent<Rigidbody2D>().mass / (distance * distance);
-                Vector2 gravityForce = (otherSpaceObject.transform.position - spaceObject.transform.position) / distance * gravityForceValue;
+                Vector2 gravityForce = GetGravityForce(spaceObject.Position, spaceObject.Mass, otherSpaceObject.Position, otherSpaceObject.Mass);
                 resultGravityForce += gravityForce;
             }
             spaceObject.GetComponent<Rigidbody2D>().AddForce(resultGravityForce);
         }
+   }
+
+    private void ShowPath()
+    {
+        Vector2[] currentPosition = new Vector2[spaceObjects.Count];
+        Vector2[] currentVelocity = new Vector2[spaceObjects.Count];
+        for (int i = 0; i < spaceObjects.Count; ++i)
+        {
+            currentPosition[i] = spaceObjects[i].Position;
+            currentVelocity[i] = spaceObjects[i].Velocity;
+        }
+        for (int i = 0; i < SpaceObject.PathDotCount * 25; ++i)
+        {
+            Vector2[] nextPosition = new Vector2[spaceObjects.Count];
+            Vector2[] nextVelocity = new Vector2[spaceObjects.Count];
+
+            for (int j = 0; j < spaceObjects.Count; ++j)
+            {
+                Vector2 resultVelocity = currentVelocity[j];
+                Vector2 resultPosition = currentPosition[j];
+                for (int k = 0; k < spaceObjects.Count; ++k)
+                {
+                    if (spaceObjects[j] == spaceObjects[k])
+                        continue;
+
+                    Vector2 gravityForce = GetGravityForce(currentPosition[j], spaceObjects[j].Mass, currentPosition[k], spaceObjects[k].Mass);
+                    resultVelocity += gravityForce / spaceObjects[j].Mass * Time.fixedDeltaTime;
+                }
+
+                nextVelocity[j] = resultVelocity;
+                resultPosition += nextVelocity[j] * Time.fixedDeltaTime;
+                nextPosition[j] = resultPosition;
+                if ((i + 1) % 25 == 0)
+                {
+                    spaceObjects[j].SetPathDot(((i + 1) / 25) - 1, resultPosition);
+                }
+            }
+            currentPosition = nextPosition;
+            currentVelocity = nextVelocity;
+        }
+    }
+
+    private Vector2 GetGravityForce(Vector2 position, float mass, Vector2 affectingPosition, float affectingMass)
+    {
+        float distance = Vector2.Distance(position, affectingPosition);
+        float gravityForceValue = G * mass * affectingMass / (distance * distance);
+        return (affectingPosition - position) / distance * gravityForceValue;
     }
 
     private void Update()
--- Assets/Scripts/SpaceObject.cs
+++ Assets/Scripts/SpaceObject.cs
@@ -6,14 +6,47 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class SpaceObject : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    public static int PathDotCount = 20;
+    private GameObject[] path = new GameObject[PathDotCount];
+
+    private void Awake()
     {
-        if (collision.rigidbody.mass >= GetComponent<Rigidbody2D>().mass)
+        for (int i = 0; i < path.Length; ++i)
         {
-            GameObject explosion = (GameObject)GameObject.Instantiate(Resources.Load("Explosion"));
-            explosion.transform.position = transform.position;
-            GameManager.Instance.RemoveSpaceObject(this);
-            Destroy(gameObject);
+            path[i] = (GameObject)GameObject.Instantiate(Resources.Load("PathDot"));
+            path[i].transform.parent = transform;
+            path[i].transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
         }
-    }        
+    }
+
+    public void SetPathDot(int number, Vector2 position)
+    {
+        path[number].transform.localPosition = position - (Vector2)transform.position; 
+    }
+
+    public float Mass
+    {
+        get { return GetComponent<Rigidbody2D>().mass; }
+    }
+
+    public Vector2 Position
+    {
+        get { return transform.position; }
+    }
+
+    public Vector2 Velocity
+    {
+        get { return GetComponent<Rigidbody2D>().velocity; }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.rigidbody.mass >= GetComponent<Rigidbody2D>().mass)
+    //    {
+    //        GameObject explosion = (GameObject)GameObject.Instantiate(Resources.Load("Explosion"));
+    //        explosion.transform.position = transform.position;
+    //        GameManager.Instance.RemoveSpaceObject(this);
+    //        Destroy(gameObject);
+    //    }
+    //}        
 }
